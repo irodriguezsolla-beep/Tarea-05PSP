43def0d03ad885cfc65b35948f8261f9c84c0d8c
Isaac <dam26@a09eq17>
2026-09-22 10:09:53 +0200

    Lista1

--- src/Interfaz.java
+++ src/Interfaz.java
@@ -1,3 +1,27 @@
+import java.util.Scanner;
 void main() {
-
+    Scanner teclado = new Scanner(System.in);
+    System.out.println("Di un número: ");
+    String numero = teclado.next();
+    System.out.println("Selecciona el nivel(1,2,3,4): ");
+    String nivel = teclado.next();
+    // Lógica según el nivel seleccionado
+    switch (nivel) {
+        case "1":
+            System.out.println("Has selecionado el nivel: " + nivel);
+            break;
+        case "2":
+            System.out.println("Has selecionado el nivel: " + nivel);
+            break;
+        case "3":
+            System.out.println("Has selecionado el nivel: " + nivel);
+            break;
+        case "4":
+            System.out.println("Has selecionado el nivel: " + nivel);
+            break;
+        default:
+            break;
+    }
 }
+
+
