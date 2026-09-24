1de21b16da587e7d3e63e9eb10cc2d1ee5b0e9d5
Isaac <dam26@a09eq17>
2026-09-24 14:25:31 +0200

    Lista1.3

--- src/Interfaz.java
+++ src/Interfaz.java
@@ -2,33 +2,41 @@ import java.sql.SQLOutput;
 import java.util.Scanner;
 void main() {
     Scanner teclado = new Scanner(System.in);
+    while (true){
+        System.out.println("Selecciona el nivel(1,2,3,4) o escribe salir: ");
+        String nivelTexto = teclado.next();
+        //si escribes salir sales
+        //nivel.equalsIgnoreCase("salir") sirve para ignorar mayusculas
+        if (nivelTexto.equalsIgnoreCase("salir")) {
+            System.out.println("¡Saliendo del programa!");
+            break;
+        }
 
-    System.out.println("Selecciona el nivel(1,2,3,4): ");
-    String nivel = teclado.next();
+        try {
+            int nivel = Integer.parseInt(nivelTexto);
+            System.out.println("Di un número: ");
+            String numero = teclado.next();
+            //Lista de niveles
+            switch (nivel) {
+                case 1:
+                    System.out.println("Nivel "+ nivel);
+                    System.out.println("Numero " + numero);
+                case 2:
+                    System.out.println("Nivel "+ nivel);
+                    System.out.println("Numero " + numero);
+                case 3:
+                    System.out.println("Nivel "+ nivel);
+                    System.out.println("Numero " + numero);
+                case 4:
+                    System.out.println("Nivel "+ nivel);
+                    System.out.println("Numero " + numero);
+                default:
+                    System.out.println("Opción no válida. Por favor, selecciona 1, 2, 3, 4 o salir.");
+                    break;
+            }
+        } catch (NumberFormatException Error){
 
-    System.out.println("Di un número: ");
-    String numero = teclado.next();
-    // Lógica según el nivel seleccionado
-    switch (nivel) {
-        case "1":
-            System.out.println("Nivel "+ nivel);
-            System.out.println("Numero " + numero);
-
-            break;
-        case "2":
-            System.out.println("Nivel "+ nivel);
-            System.out.println("Numero " + numero);
-            break;
-        case "3":
-            System.out.println("Nivel "+ nivel);
-            System.out.println("Numero " + numero);
-            break;
-        case "4":
-            System.out.println("Nivel "+ nivel);
-            System.out.println("Numero " + numero);
-            break;
-        default:
-            break;
+        }
     }
 }
 
