5cedc28f3ddfe5df8e0e6f9e001f52ff85bf18f0
Isaac <dam26@a09eq17>
2026-09-24 14:08:06 +0200

    Lista1.2

--- src/Interfaz.java
+++ src/Interfaz.java
@@ -1,30 +1,31 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 void main() {
     Scanner teclado = new Scanner(System.in);
 
     System.out.println("Selecciona el nivel(1,2,3,4): ");
     String nivel = teclado.next();
+
+    System.out.println("Di un número: ");
+    String numero = teclado.next();
     // Lógica según el nivel seleccionado
     switch (nivel) {
         case "1":
-            System.out.println("Has selecionado el nivel: " + nivel);
-            System.out.println("Di un número: ");
-            String numero1 = teclado.next();
+            System.out.println("Nivel "+ nivel);
+            System.out.println("Numero " + numero);
+
             break;
         case "2":
-            System.out.println("Has selecionado el nivel: " + nivel);
-            System.out.println("Di un número: ");
-            String numero2 = teclado.next();
+            System.out.println("Nivel "+ nivel);
+            System.out.println("Numero " + numero);
             break;
         case "3":
-            System.out.println("Has selecionado el nivel: " + nivel);
-            System.out.println("Di un número: ");
-            String numero3 = teclado.next();
+            System.out.println("Nivel "+ nivel);
+            System.out.println("Numero " + numero);
             break;
         case "4":
-            System.out.println("Has selecionado el nivel: " + nivel);
-            System.out.println("Di un número: ");
-            String numero4 = teclado.next();
+            System.out.println("Nivel "+ nivel);
+            System.out.println("Numero " + numero);
             break;
         default:
             break;
